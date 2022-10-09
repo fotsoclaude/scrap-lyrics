8988df46e54665a9e055943a966ecb4d398e4c9b
FOTSO Claude <batfkc@gmail.com>
2022-10-09 07:54:29 +0100

    On obtient le lyrics d'une chanson

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from pprint import pprint
-import requests
 
+import requests
+from bs4 import BeautifulSoup
+
+def extract_lyrics(url):
+    r = requests.get(url)
+    
+    if r.status_code != 200:
+        print("Page inaccessible.")
+        return []
+    
+    soup = BeautifulSoup(r.content, 'html.parser')
+    lyrics = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-6")
+    
+    if not lyrics:
+        return extract_lyrics(url)
+    
+    for sentence in lyrics.stripped_strings:
+        print(sentence)
 
 def get_all_urls():
     links = []
@@ -18,5 +35,6 @@ def get_all_urls():
         links.extend([song.get("url") for song in songs])
         
         next_page = response.get("next_page")
+    print(links)
 
-get_all_urls()
+extract_lyrics(url="https://genius.com/Adele-i-drink-wine-debuted-live-lyrics")
